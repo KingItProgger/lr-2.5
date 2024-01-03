9b70608a521bc9d788d21f81d74b8580a81f25dc
Yaroslav <soloben095@gmail.com>
2024-01-03 11:47:31 +0300

    laba

---
+++ pycharm/task1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+
+    a = tuple(map(int, input().split()))
+
+    l = 0
+    for i, item in enumerate(a):
+        if i < len(a)-1:
+            if item == a[i+1]:
+                l = i +2
+                break
+        else:
+            break
+
+    print(a[l:])
